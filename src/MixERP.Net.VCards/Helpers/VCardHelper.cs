0b1babf2d7719c3713f84e13bd505f17f1359ad1
NightOwl <midnightow1@outlook.com>
2022-08-19 21:33:51 +0800

    Multi-target .NET Standard 1.4/2.0 and .NET 5
    Bump version to 1.0.1

--- src/MixERP.Net.VCards/Helpers/VCardHelper.cs
+++ src/MixERP.Net.VCards/Helpers/VCardHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace MixERP.Net.VCards.Helpers
 {
@@ -7,7 +8,13 @@ namespace MixERP.Net.VCards.Helpers
         public static string[] SplitCards(string contents)
         {
             MatchCollection matchCollection = Regex.Matches(contents, "((BEGIN:VCARD)(.*?)(END:VCARD))", RegexOptions.Singleline);
+#if NETSTANDARD
+            return matchCollection.Cast<Match>().Select(m => m.Value).ToArray();
+#elif NET5_0_OR_GREATER
             return matchCollection.Select(m => m.Value).ToArray();
+#else
+#error This code block does not match csproj TargetFrameworks list
+#endif
         }
     }
 }
